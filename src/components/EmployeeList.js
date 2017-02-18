6250b95b5d32b2228fe50010f4cce32386cf464b
VahanAper <asryanvahandev@gmail.com>
2017-02-19 02:50:46 +0400

    create form finished

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>employee list</Text>
+        <Text>employee list</Text>
+        <Text>employee list</Text>
+        <Text>employee list</Text>
+        <Text>employee list</Text>
+        <Text>employee list</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
